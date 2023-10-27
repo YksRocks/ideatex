743950fdfe9d14e06e9d1f3cfcd7b86786dc690d
YksRocks <73733249+YksRocks@users.noreply.github.com>
2023-10-27 19:29:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
   cookies: {
+    sameSite:"none",
     httpOnly: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
