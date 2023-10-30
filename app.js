b9f4dd973a37123766a678a7e4337f44ba624aa2
YksRocks <73733249+YksRocks@users.noreply.github.com>
2023-10-31 00:27:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -125,9 +125,9 @@ app.post("/updatee", async (req, res) => {
 
 
 app.post("/logout", (req, res) => {
-  res.cookie('user','', { expires: new Date(0) });
-  res.cookie('s1','', { expires: new Date(0) });
-  res.cookie('s2','', { expires: new Date(0) });
+  res.cookie('user','', { expires: new Date(0),sameSite: 'None' });
+  res.cookie('s1','', { expires: new Date(0) ,sameSite: 'None'});
+  res.cookie('s2','', { expires: new Date(0),sameSite: 'None' });
   res.json("logedOut");
 });
 
