ad917ec680fde66ef25abcaaea827fff70dcf55e
YksRocks <73733249+YksRocks@users.noreply.github.com>
2023-11-02 14:29:24 +0530

    Update Home.jsx

--- client/views/Home.jsx
+++ client/views/Home.jsx
@@ -3,20 +3,20 @@ import Form from "../src/Form";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import HashLoader from "react-spinners/HashLoader";
+// import HashLoader from "react-spinners/HashLoader";
 // import { useLocation, useNavigate } from "react-router-dom";
 // import { BASE_URL_FE } from "../src/camponents/base";
 const BASE_URL_FE="https://ideatex.onrender.com";
 export default function Home() {
-  const color = "#646cff";
+  // const color = "#646cff";
   const [validd, setValidd] = useState(false);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const [s1, setS1] = useState(0);
   const [s2, setS2] = useState(0);
   const navigate = useNavigate();
   axios.defaults.withCredentials = true;
   useEffect(() => {
-    setLoading(true);
+    // setLoading(true);
     axios
       .post(`https://ideatex.onrender.com/`)
       .then((res) => {
@@ -24,9 +24,9 @@ export default function Home() {
           setValidd(true);
           setS1(res.data.s1);
           setS2(res.data.s2);
-          setTimeout(() => {
-            setLoading(false);
-          }, 4000);
+          // setTimeout(() => {
+          //   setLoading(false);
+          // }, 4000);
         } else {
           navigate("/login");
         }
@@ -36,17 +36,6 @@ export default function Home() {
   return (
     <>
       {validd && (
-        <div>
-          {loading ? (
-            <HashLoader
-              color={color}
-              loading={loading}
-              size={50}
-              aria-label="Loading Spinner"
-              data-testid="loader"
-            />
-          ) : (
-            <>
               <div>
                 <Hero validd={validd} />
               </div>
@@ -55,10 +44,7 @@ export default function Home() {
                   <Form validd={validd} s1={s1} s2={s2} />
                 </main>
               </div>
-            </>
           )}
-        </div>
-      )}
     </>
   );
 }
